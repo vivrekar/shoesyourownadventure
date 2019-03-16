d4cb23bb19b69a03778d2e3c8fc54f9fbdbf96f9
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2019-03-15 19:41:49 -0700

    improve splash screen code

--- ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/SplashActivity.java
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/SplashActivity.java
@@ -5,28 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
 
-        Thread welcomeThread = new Thread() {
-
-            @Override
-            public void run() {
-                try {
-                    super.run();
-                    sleep(2500);  //Delay of 2.5 seconds
-                } catch (Exception e) {
-
-                } finally {
-                    Intent i = new Intent(SplashActivity.this,
-                            MainActivity.class);
-                    startActivity(i);
-                    finish();
-                }
-            }
-        };
-        welcomeThread.start();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
