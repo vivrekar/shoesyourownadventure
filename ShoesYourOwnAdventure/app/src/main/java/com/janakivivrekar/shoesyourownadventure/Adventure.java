3e8b2b12a33495f3cb0ead9c96f3f3f8da8fcb43
tonyanguyen <tonyanguyen@berkeley.edu>
2019-03-18 20:13:58 -0700

    adding checkbox listener, created method for instantiating adventure attribute

--- ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/Adventure.java
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/Adventure.java
@@ -1,9 +1,15 @@
 package com.janakivivrekar.shoesyourownadventure;
 
-import java.util.ArrayList;
+import java.util.Set;
 
 public class Adventure {
-    int radius = 0;
-    boolean round_trip = false;
-    ArrayList<AdventureAttribute> attributes = new ArrayList();
+    int radius;
+    boolean round_trip;
+    Set<AdventureAttribute> attributes;
+
+    public Adventure(int radius, boolean round_trip, Set<AdventureAttribute> attributes) {
+        this.radius = radius;
+        this.round_trip = round_trip;
+        this.attributes = attributes;
+    }
 }
--- ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/AdventureAttribute.java
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/AdventureAttribute.java
@@ -5,4 +5,10 @@ import java.util.ArrayList;
 public class AdventureAttribute {
     String name;
     boolean selected;
+
+    public AdventureAttribute(String name, boolean selected) {
+        this.name = name;
+        this.selected = selected;
+    }
+
 }
