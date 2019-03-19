dab3188765b593988f58b3ce8a4b5ca366ca21e7
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2019-03-19 06:01:06 -0700

    wire up the welcome screen.

--- ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/MainActivity.java
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/MainActivity.java
@@ -1,7 +1,10 @@
 package com.janakivivrekar.shoesyourownadventure;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_screen);
+        Button beginButton = findViewById(R.id.begin_button);
+        beginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nextActivity = new Intent(MainActivity.this, ConnectShoesActivity.class);
+                startActivity(nextActivity);
+            }
+        });
     }
 }
