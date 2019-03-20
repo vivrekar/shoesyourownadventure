7d3f7e48fa1260a098ee76ffb8a6efe809534d6b
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2019-03-19 17:47:34 -0700

    layouts from tonya

---
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/AdventureAttribute.java
@@ -0,0 +1,14 @@
+package com.janakivivrekar.shoesyourownadventure;
+
+import java.util.ArrayList;
+
+public class AdventureAttribute {
+    String name;
+    boolean selected;
+
+    public AdventureAttribute(String name, boolean selected) {
+        this.name = name;
+        this.selected = selected;
+    }
+
+}
