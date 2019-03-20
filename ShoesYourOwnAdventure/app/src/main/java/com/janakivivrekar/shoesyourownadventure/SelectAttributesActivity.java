c2a52560fb97de751756ca19f41177fff3abf86b
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2019-03-19 20:47:52 -0700

    add active adventure and take break screen

--- ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/SelectAttributesActivity.java
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/SelectAttributesActivity.java
@@ -1,9 +1,9 @@
 package com.janakivivrekar.shoesyourownadventure;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.CheckBox;
-import android.widget.ScrollView;
 
 public class SelectAttributesActivity extends AppCompatActivity {
 
@@ -11,13 +11,14 @@ public class SelectAttributesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.select_attributes_screen);
-        ScrollView attributesScrollView = findViewById(R.id.attributes_scroll_view);
+        Intent nextActivity = new Intent(this, ActiveAdventureActivity.class);
+        startActivity(nextActivity);
+        /*ScrollView attributesScrollView = findViewById(R.id.attributes_scroll_view);
         CheckBox nature = findViewById(R.id.nature);
         if (nature.isChecked()) {
             nature.setChecked(true);
             createAttribute(nature, true);
-        }
-
+        }*/
     }
 
     public void createAttribute(CheckBox checkbox, boolean isChecked) {
