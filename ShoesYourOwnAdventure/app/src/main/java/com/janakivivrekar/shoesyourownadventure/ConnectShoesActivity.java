5458e4f139620976a476f1efdff2f2ec3e8b43f5
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2019-03-19 17:21:42 -0700

    Bluetooth connection (#5)
    implement bluetooth le connection to feather

---
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/ConnectShoesActivity.java
@@ -0,0 +1,242 @@
+package com.janakivivrekar.shoesyourownadventure;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import java.util.Set;
+
+public class ConnectShoesActivity extends AppCompatActivity {
+    private final static String TAG = ConnectShoesActivity.class.getSimpleName();
+    private TextView connectionStatusText;
+    private Button connectToNextButton;
+
+    private BluetoothDevice targetDevice;
+    private BluetoothAdapter myBluetooth = null;
+    private BluetoothLeService mBluetoothLeService;
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(targetDevice.getAddress());
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.connect_shoes_screen);
+
+        // Set up UI References
+        connectionStatusText = (TextView) findViewById(R.id.connection_status);
+        connectToNextButton = (Button) findViewById(R.id.connect_to_next);
+        connectToNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { connect(); }
+        });
+
+        // TODO: Check permissions!
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if(myBluetooth == null) {
+            Toast.makeText(getApplicationContext(), "Sorry, this phone doesn't support bluetooth!", Toast.LENGTH_LONG).show();
+            finish();
+        } else if (!myBluetooth.isEnabled()) {
+            //Ask to the user turn bluetooth on
+            Intent requestEnableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(requestEnableBluetooth,1);
+        }
+
+        // Check paired devices and retrieve target device.
+        if (!checkPairedDevicesForTarget()) {finish();}
+
+        targetDevice.getName();
+        targetDevice.getAddress();
+        }
+
+    /*
+     * Helper function to check already paired devices and set the target device.
+     * */
+    private boolean checkPairedDevicesForTarget() {
+        Set<BluetoothDevice> pairedDevices = myBluetooth.getBondedDevices();
+        if (pairedDevices.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            ArrayList<BluetoothDevice> pairedDevicesList = new ArrayList();
+            pairedDevicesList.addAll(pairedDevices);
+            // Set target device to the first paired device (expected to be Feather)
+            // TODO: create a screen for user to select which device to connect.
+            targetDevice = pairedDevicesList.get(0);
+            return true;
+        }
+    }
+
+    /*
+     * Helper function to bind with BluetoothLeService and connect with targetDevice.
+     * */
+    private boolean connect() {
+        // Bind with the BluetoothLeService
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        boolean bindServiceResult = bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+        if (!bindServiceResult) { return false; }
+
+
+        return true;
+    }
+
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
+    //                        or notification operations.
+    private boolean mConnected = false;
+    private ArrayList<ArrayList<BluetoothGattCharacteristic>> mGattCharacteristics =
+            new ArrayList<ArrayList<BluetoothGattCharacteristic>>();
+    private BluetoothGattCharacteristic mNotifyCharacteristic;
+
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                updateConnectionState(R.string.connected);
+                invalidateOptionsMenu();
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                updateConnectionState(R.string.disconnected);
+                invalidateOptionsMenu();
+            }
+        }
+    };
+
+    // If a given GATT characteristic is selected, check for supported features.  This sample
+    // demonstrates 'Read' and 'Notify' features.  See
+    // http://d.android.com/reference/android/bluetooth/BluetoothGatt.html for the complete
+    // list of supported characteristic features.
+    private final ExpandableListView.OnChildClickListener servicesListClickListner =
+            new ExpandableListView.OnChildClickListener() {
+                @Override
+                public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,
+                                            int childPosition, long id) {
+                    if (mGattCharacteristics != null) {
+                        final BluetoothGattCharacteristic characteristic =
+                                mGattCharacteristics.get(groupPosition).get(childPosition);
+                        final int charaProp = characteristic.getProperties();
+                        if ((charaProp | BluetoothGattCharacteristic.PROPERTY_READ) > 0) {
+                            // If there is an active notification on a characteristic, clear
+                            // it first so it doesn't update the data field on the user interface.
+                            if (mNotifyCharacteristic != null) {
+                                mBluetoothLeService.setCharacteristicNotification(
+                                        mNotifyCharacteristic, false);
+                                mNotifyCharacteristic = null;
+                            }
+                            mBluetoothLeService.readCharacteristic(characteristic);
+                        }
+                        if ((charaProp | BluetoothGattCharacteristic.PROPERTY_NOTIFY) > 0) {
+                            mNotifyCharacteristic = characteristic;
+                            mBluetoothLeService.setCharacteristicNotification(
+                                    characteristic, true);
+                        }
+                        return true;
+                    }
+                    return false;
+                }
+            };
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(targetDevice.getAddress());
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
+
+
+    private void updateConnectionState(final int resourceId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                // Update connection status text field.
+                connectionStatusText.setText(resourceId);
+                // Update button functionality.
+                if (resourceId == R.string.connected) {
+                    connectToNextButton.setText(R.string.next);
+                    connectToNextButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent nextActivity = new Intent(ConnectShoesActivity.this, SelectAttributesActivity.class);
+                            startActivity(nextActivity);
+                        }
+                    });
+                } else {
+                    connectToNextButton.setText(R.string.connect);
+                    connectToNextButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            connect();
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
+    }
+
+}
+
