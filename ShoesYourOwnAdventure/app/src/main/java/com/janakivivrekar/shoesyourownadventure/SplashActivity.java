45a82b6a85449d2cd551b10d7c27e63c9e8711ee
Janaki Vivrekar <janaki.vivrekar@berkeley.edu>
2019-03-20 09:56:40 -0700

    formatting

--- ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/SplashActivity.java
+++ ShoesYourOwnAdventure/app/src/main/java/com/janakivivrekar/shoesyourownadventure/SplashActivity.java
@@ -9,6 +9,7 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
         Thread welcomeThread = new Thread() {
 
             @Override
